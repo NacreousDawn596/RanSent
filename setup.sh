99c33cf4bea3f2d0a80d9823d2e5589d9f7d4070
NacreousDawn596 <75474839+NacreousDawn596@users.noreply.github.com>
2021-10-23 14:59:58 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,11 @@
+if [ "$1" == "" ]; then
+  echo "usage:
+    ./setup python3 : to run the python script
+    ./setup nodejs : to run the js file
+  enjoy! :)"
+  exit
+fi
+
 echo "installing packages..."
 sleep 1
 sudo apt-get install nodejs
