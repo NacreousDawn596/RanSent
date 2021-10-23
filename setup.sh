f4aed07a327cc8d84807dc03a2d9c3ac4089bcb9
NacreousDawn596 <75474839+NacreousDawn596@users.noreply.github.com>
2021-10-23 01:01:17 +0100

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,9 @@
+echo "installing packages..."
+sleep 1
+sudo apt-get install nodejs
+clear
+sudo pacman -S nodejs
+clear
+sudo dnf install nodejs
+clear
+echo 'now you can run it by writing "node main.js" enjoy!'
